592ca39faa9d6da9092c6f641744f913d5180366
blockodyssey-cheonga <cheonga@blockodyssey.io>
2022-08-30 17:25:32 +0900

    react-router-dom 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="App">App</div>;
-}
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/home" element={<div>home</div>} />
+          <Route path="/mypage" element={<div>mypage</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
